1d53dbc8565129a222847cbd303c76186277e0a2
nguyenphuanbmt113 <nguyenphuanbmt113@gmail.com>
2022-07-17 21:05:12 +0700

    setup taiwind

---
+++ tailwind.config.js
@@ -0,0 +1,38 @@
+module.exports = {
+  mode: "jit",
+  purge: ["./*.html"],
+  darkMode: "class", // or 'media' or 'class'
+  theme: {
+    screens: {
+      "2xl": { max: "1535px" },
+      // => @media (max-width: 1535px) { ... }
+
+      xl: { max: "1279px" },
+      // => @media (max-width: 1279px) { ... }
+
+      lg: { max: "1023px" },
+      // => @media (max-width: 1023px) { ... }
+
+      md: { max: "767px" },
+      // => @media (max-width: 767px) { ... }
+
+      sm: { max: "639px" },
+      // => @media (max-width: 639px) { ... }
+    },
+    extend: {
+      spacing: {
+        35: "35px",
+      },
+      colors: {
+        gray33: "#333",
+        orangefa: "#ffa400",
+      },
+    },
+  },
+  variants: {
+    extend: {
+      backgroundColor: ["odd"],
+    },
+  },
+  plugins: [],
+};
